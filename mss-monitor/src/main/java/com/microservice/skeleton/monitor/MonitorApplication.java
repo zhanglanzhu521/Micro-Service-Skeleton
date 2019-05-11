ad73e98aa935669b884949a10d590d1be214e6cb
zhanglanzhu <512312126@qq.com>
2019-05-11 12:12:10 +0800

    d

--- mss-monitor/src/main/java/com/microservice/skeleton/monitor/MonitorApplication.java
+++ mss-monitor/src/main/java/com/microservice/skeleton/monitor/MonitorApplication.java
@@ -4,14 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
-import zipkin.server.EnableZipkinServer;
+//import org.springframework.cloud.netflix.turbine.EnableTurbine;
+//import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrixDashboard
-@EnableTurbine
-@EnableZipkinServer
+//@EnableTurbine
+//@EnableZipkinServer
 public class MonitorApplication {
 
 	public static void main(String[] args) {
